0aac58e69eeddcb3b51873508671bc6ddcc3ad15
Ernesto De Los Santos <netos_id@yahoo.com>
2016-09-22 01:11:56 -0500

    Getting repo linked.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Main from './lib/main'
+import { Router, Route, browserHistory } from 'react-router'
+import Main from './lib/Main'
+import Detail from './lib/Detail'
+import Preview from './lib/Preview'
 
 render((
-  <Main name="Boilerplate"/>
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}/>
+    <Route path="/Detail/:image/:name/:email/:phone/:location" component={Detail}/>
+  </Router>
 ), document.getElementById('app'))
